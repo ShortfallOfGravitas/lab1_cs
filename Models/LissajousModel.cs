e8f67dc198367835cf28631467da50defc7983d7
Nazarii <nazarkarac8@gmail.com>
2026-09-07 22:25:12 +0300

    corrected struct

--- Models/LissajousModel.cs
+++ Models/LissajousModel.cs
@@ -3,42 +3,43 @@ using System.Collections.Generic;
 
 namespace LissajousApp.Models;
 
-public struct Coordinate
+public readonly struct Coordinate(double x, double y)
 {
-    public double X { get; set }    //треба розібратись
-    public double Y {get; set; }
-
-    public Coordinate(double x, double y)
-    {
-        X = x;
-        Y = y;
-    }
+    public double X { get; } = X;   //треба розібратись
+    public double Y { get; } = Y;
 }
 
 
-public class LissajousModel()
+public class LissajousModel(double amplX, double amplY, 
+                            double freqX, double freqY, 
+                            double phX, double phY)
 {
     public List<Coordinate> PointsList = new List<Coordinate>(); //array with coordinates
     private Stack<List<Coordinate>> history = new Stack<List<Coordinate>>();
-    
+
+    private int dots = 20; //точки на найкоротший відрізок
     private double t;   //час у симуляції
     private double Dt;  // delta t (sampling rate, інтервал дискретизації) 
     
     //amplitude
-    private double Ax;
-    private double Ay;
+    private double Ax = amplX;
+    private double Ay  = amplY;
     
     //frequency
-    private double fx;
-    private double fy;
+    private double fx = freqX;
+    private double fy = freqY;
     
     //phase
-    private double px;
-    private double py;
+    private double px  = phX;
+    private double py = phY;
 
     
-    CalculateSamplRate()
+    CalculateSamplRate(a, b)
     {
-        
+        double maxFreq = Math.Max(a, b);
+
+        double periodT = 1.0 / maxFreq;
+
+        return double deltaT = periodT / dots;
     }
 }
